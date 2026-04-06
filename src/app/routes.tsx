310a419c646a708854b6e0924e8825974371ca18
HanyuZhu-1220 <hzhu103@asu.edu>
2026-04-06 09:53:45 -0400

    update

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -8,18 +8,23 @@ import { FundActivity } from "./components/FundActivity";
 import { HelpCenter } from "./components/HelpCenter";
 import { UserProfile } from "./components/UserProfile";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      Component: Layout,
+      children: [
+        { index: true, Component: Dashboard },
+        { path: "apply", Component: ApplyCard },
+        { path: "cards", Component: CardList },
+        { path: "transactions", Component: TransactionDetails },
+        { path: "funds", Component: FundActivity },
+        { path: "help", Component: HelpCenter },
+        { path: "profile", Component: UserProfile },
+      ],
+    },
+  ],
   {
-    path: "/",
-    Component: Layout,
-    children: [
-      { index: true, Component: Dashboard },
-      { path: "apply", Component: ApplyCard },
-      { path: "cards", Component: CardList },
-      { path: "transactions", Component: TransactionDetails },
-      { path: "funds", Component: FundActivity },
-      { path: "help", Component: HelpCenter },
-      { path: "profile", Component: UserProfile },
-    ],
+    basename: import.meta.env.BASE_URL,
   },
-]);
+);
